b5725a56a93711e9e5a95f98b49a598cdee11edb
Andres Quintero <andresquintero06@gmail.com>
2022-11-05 17:57:29 -0500

    Se esta implementando el final del Camino para el agente

--- Assets/Scripts/Camino.cs
+++ Assets/Scripts/Camino.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Camino : MonoBehaviour
 {
     public GameObject salidas;
+    int localesIngresados = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +18,40 @@ public class Camino : MonoBehaviour
 
         Vector3 v = listaSalidas[salidaEscogida].transform.position;
         bool v1 = this.GetComponent<NavMeshAgent>().SetDestination(v);
+
+       
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Invoke("Salir", 10.0f);
+            
+        }
     }
+
+    void Salir()
+    {
+        this.GetComponent<NavMeshAgent>().speed = Random.Range(0, 8);
+        GameObject[] listaSalidas = GameObject.FindGameObjectsWithTag("salida_cc");
+        int salidaEscogida = Random.Range(0, listaSalidas.Length);
+
+        Vector3 v = listaSalidas[salidaEscogida].transform.position;
+        bool v1 = this.GetComponent<NavMeshAgent>().SetDestination(v);
+
+        var clones = GameObject.FindGameObjectsWithTag("salida_cc");
+        if (clones.Any())
+        {
+            foreach (var clone in clones)
+            {
+                Destroy(clone);
+            }
+        }
+
+
+    }
+
 }
