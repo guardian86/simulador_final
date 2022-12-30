82753e274f0a7d0041592625da3e00dbdb538775
Andres Quintero <andresquintero06@gmail.com>
2022-12-29 22:29:20 -0500

    Se cambia al administrador delegar la creacion del agente inicial

---
+++ Assets/Scripts/Administrador.cs
@@ -0,0 +1,131 @@
+using System;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+using static UnityEngine.ParticleSystem;
+
+
+
+
+[Serializable]
+public class Administrador : MonoBehaviour
+{
+
+    /// <summary>
+    /// Funciones que va a realizar el administrador
+    /// Crear unicamente el Agente 
+    /// Llevar el control de los agentes para generar el reporte json con la info recibida
+    /// </summary>
+
+    #region "Variables de Publicas"
+    public GameObject persona;
+    public GameObject puntoInicio;
+    public int AforoMaximo;
+    #endregion
+
+    #region "Variables Privadas"
+    int contadorAgentes = 0;
+    private GameObject clon;
+    int contAgentesCovid = 0;
+
+    EmissionModule emision;
+    private string json;
+    #endregion
+
+
+    // Update is called once per frame
+    private void Start()
+    {
+        Invoke("CrearAgente", 2f);
+    }
+
+    void Update()
+    {
+
+
+    }
+
+    void CrearAgente()
+    {
+        try
+        {
+            if (contadorAgentes < AforoMaximo )
+            {
+
+                clon = Instantiate(persona, puntoInicio.transform.position, puntoInicio.transform.rotation);
+                clon.GetComponentInChildren<Camino>().enabled = true;
+                clon.GetComponentInChildren<Particula>().enabled = true;
+                clon.gameObject.SetActive(true);
+
+                //clon.GetComponentInChildren<ParticleSystem>().Play(true);
+                var probCovid = UnityEngine.Random.Range(0, 100);
+
+                if (probCovid > 80) clon.GetComponentInChildren<ParticleSystem>().Play(true);
+                else clon.GetComponentInChildren<ParticleSystem>().Stop(true);
+
+
+                Debug.Log("CrearAgente " + contadorAgentes);
+                contadorAgentes++;
+                Invoke("CrearAgente", 2f);
+            }
+        }
+        catch (System.Exception ex)
+        {
+            Debug.Log(ex.Message.ToString());
+        }
+    }
+
+
+    private void ObtenerAgentes()
+    {
+        GameObject[] listAgentes = GameObject.FindGameObjectsWithTag("Agentes");
+        foreach (GameObject agente in listAgentes)
+        {
+            //agente.SetActive(true);
+            emision = agente.gameObject.GetComponent<ParticleSystem>().emission;
+            if (emision.enabled)
+            {
+                contAgentesCovid++;
+            }
+        }
+        //var x = listAgentes[listAgentes.Length - 1].transform.gameObject.active;
+        //ParticleSystem[] y = emision = ParticleSystem.FindObjectsOfType<ParticleSystem>();
+
+        //emision = GetComponent<List<ParticleSystem>>().Count();
+
+        if (listAgentes.Any())
+        {
+            using (StreamReader r = new StreamReader("Assets/JSON/ReporteAgentes.json"))
+            {
+                json = r.ReadToEnd();
+                Debug.Log(json);
+                Administrador x = CreateFromJSON(json);
+
+                //Debug.Log(x[""][""]);
+            }
+        }
+        
+    }
+
+
+    public void Load(string savedData)
+    {
+        JsonUtility.FromJsonOverwrite(savedData, this);
+    }
+
+
+    //generar el json a exportar 
+    public string SaveToString()
+    {
+        return JsonUtility.ToJson(this);
+    }
+
+
+    public static Administrador CreateFromJSON(string jsonString)
+    {
+        return JsonUtility.FromJson<Administrador>(jsonString);
+    }
+
+
+
+}
--- Assets/Scripts/Agente.cs
+++ Assets/Scripts/Agente.cs
@@ -2,18 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.AI;
 using static UnityEngine.ParticleSystem;
 
 public class Agente : MonoBehaviour
 {
 
     #region "Variables de Entorno"
-    public GameObject persona;
-    public GameObject puntoInicio;
     private EmissionModule AgenteEmisor;
-    private GameObject clon;
-    public int AforoMaximo;
-    int contadorAgentes = 0;
+    
+    
+    
     List<AgentesContagiados> agenteCovid19;
     int contadorCovid = 1;
     #endregion
@@ -21,46 +20,22 @@ public class Agente : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("CrearAgente", 2f);
+        GameObject[] listaAgentes = GameObject.FindGameObjectsWithTag("tagPersonas");
+        int salidaEscogida = Random.Range(0, listaAgentes.Length);
+
+        //Vector3 v = listaAgentes[salidaEscogida].transform.position;
+        //this.GetComponent<NavMeshAgent>().SetDestination(v);
+        //Invoke("CrearAgente", 2f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (contadorAgentes > 1) Invoke("ActualizarEstadoParticulaClon", 6f);
+        //if (contadorAgentes > 1) Invoke("ActualizarEstadoParticulaClon", 6f);
     }
 
     #region "Manejo Eventos y Agentes"
-    void CrearAgente()
-    {
-        try
-        {
-            if (contadorAgentes < AforoMaximo)
-            {
-
-                clon = Instantiate(persona, puntoInicio.transform.position, puntoInicio.transform.rotation);
-                clon.GetComponentInChildren<Camino>().enabled = true;
-                clon.GetComponentInChildren<Particula>().enabled = true;
-                clon.gameObject.SetActive(true);
-
-                //clon.GetComponentInChildren<ParticleSystem>().Play(true);
-                var probCovid = Random.Range(0, 100);
-
-                if (probCovid > 80) clon.GetComponentInChildren<ParticleSystem>().Play(true);
-                else clon.GetComponentInChildren<ParticleSystem>().Stop(true);
-
-                Debug.Log("CrearAgente " + contadorAgentes);
-                contadorAgentes++;
-                Invoke("CrearAgente", 2f);
-            }
-
-        }
-        catch (System.Exception ex)
-        {
-            Debug.Log(ex.Message.ToString());
-        }
-    }
-
+  
     private void ActivarDispararParticula()
     {
         if (!AgenteEmisor.enabled && agenteCovid19.Any())
@@ -71,16 +46,22 @@ public class Agente : MonoBehaviour
             AgenteEmisor.enabled = false;
     }
 
+
+    public void ActualizarEstadoParticulaXAgente()
+    {
+
+    }
+
     public void ActualizarEstadoParticulaClon()
     {
 
         try
         {
             agenteCovid19 = new List<AgentesContagiados>();
-            if (contadorAgentes > 1)
-            {
+            //if (contadorAgentes > 1)
+            //{
                 
-                AgenteEmisor = clon.GetComponentInChildren<ParticleSystem>().emission;
+                //AgenteEmisor = clon.GetComponentInChildren<ParticleSystem>().emission;
                 if (AgenteEmisor.enabled) //IsUnityNull() AgenteEmisor.ToString().Count() > 0
                 {
                     agenteCovid19.Add(new AgentesContagiados() { cantidadContagiados = contadorCovid, tieneCovid = true });
@@ -94,7 +75,7 @@ public class Agente : MonoBehaviour
                 {
                     agenteCovid19.Add(new AgentesContagiados() { cantidadContagiados = 0, tieneCovid = false });
                 }
-            }
+            //}
         }
         catch (System.Exception ex)
         {
